4c9f43047b2ddf70d3bef86d35c9f22f93607eba
KotetsuCodes <kotetsuCodes@gmail.com>
2018-12-11 22:56:06 -0600

    Added 3d model and material for coins

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -1,34 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CoinPickup : MonoBehaviour {
+public class CoinPickup : MonoBehaviour
+{
 
-    SpriteRenderer coinSprite;
+    // SpriteRenderer coinSprite;
+
     AudioSource coinSound;
     bool isDestroyed = false;
 
-	// Use this for initialization
-	void Start () {
-        coinSprite = GetComponent<SpriteRenderer>();
+    // Use this for initialization
+    void Start()
+    {
+        // coinSprite = GetComponent<SpriteRenderer>();
         coinSound = GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(isDestroyed && coinSound.isPlaying == false)
-         Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Helpers.DebugValue("coinSound.isPlaying: ", coinSound.isPlaying);
+        Helpers.DebugValue("isDestroyed:", isDestroyed);
+
+        if (isDestroyed && coinSound.isPlaying == false)
+        {
+            Debug.Log("Destroying coin");
+            Destroy(gameObject);
+        }
+
 
     }
 
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("Coin collected!");
+
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        Debug.Log("Coin collided with Player");
+
+    //        // coinSprite.enabled = false;
+    //        GameManager.instance.PlayerCoinPickup();
+    //        coinSound.Play();
+    //        isDestroyed = true;
+    //    }
+    //}
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Collision occurred");
+
         if (collision.gameObject.tag == "Player")
         {
-            coinSprite.enabled = false;
+            Debug.Log("Collision with player");
+            // coinSprite.enabled = false;
             GameManager.instance.PlayerCoinPickup();
             coinSound.Play();
             isDestroyed = true;
-        }            
+        }
+        else
+        {
+            Debug.Log(collision.gameObject.tag);
+        }
     }
 }
